ccd8b7a5c67b5fc57e2fcd9195c959e356f0e95a
YehaJeon <aheynoez@gmail.com>
2022-01-15 16:42:36 +0900

    연습

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const a=5454;
-const b=33
-let c=465
+const player = {
+ sayHello: function (otherPersonsName) {
+     console.log("hello " + otherPersonsName + " nice to meet you! ")
+ },
+};
+
+// console.log(player.name);
+player.sayHello("yeha");
+
+const daysOfWeek = [ "mon", "tue", "wed", "thu", "fri", "sat" ];
+
+console.log(daysOfWeek[0]);
 
 
+const a = 5;
+const isYehaFat = true;
 
-console.log(a+b+c);
+console.log(a);
+console.log(isYehaFat);
+
+
+const toBuy = [ "potato", "tomato", "pizza"  ];
+
+
+toBuy[2] = "water";
+
+console.log(toBuy);
+
