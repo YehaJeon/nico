b64877756180b4240ef9df634d0754d10426b35e
YehaJeon <aheynoez@gmail.com>
2022-01-17 16:39:25 +0900

    큐큐루

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const age = prompt("How old are you?")
-console.log(age)
+
+console.log(age, parseInt(age))
+
+
+console.log(typeof "15", typeof parseInt("15"))
+
+const age = parseInt(prompt("How old are you"))
+
