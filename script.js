72e1ed553653265ed90cf920f59c6f1df0ca7022
YehaJeon <aheynoez@gmail.com>
2022-01-17 13:29:50 +0900

    return

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const player = {
-    name: "Yeha",
-    age: "20",
-}
+// const player = {
+//     name: "Yeha",
+//     age: "20",
+// }
 
-console.log(player.age);
+// console.log(player.age);
 
 // function plus(a,b) {
 //  console.log(a+b); 
@@ -17,17 +17,31 @@ console.log(player.age);
 const calculater = {
     
     add: function (a,b) { 
-        console.log (a+b)
+        return (a+b)
     },
     
 
 };
 
-calculater.add(3,4)  
+// calculater.add(3,4)  
 
 console.log(calculater.add(3,4));
+ 
 
 
+// function calculater(a,b) {
+//  return a+b
+// }
 
+// calculater(3,5)
+ 
 
+const age = 96;
 
+function calculateKrAge(ageOfForeigner) {
+   return ageOfForeigner + 2 
+}
+
+const krAge = calculateKrAge(age)
+
+console.log(krAge);
