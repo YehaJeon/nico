19c2ddef14900ae12dbc06d18f7c869368f0884d
YehaJeon <aheynoez@gmail.com>
2022-01-17 13:58:05 +0900

    return oh

--- script.js
+++ script.js
@@ -1,47 +1,68 @@
-// const player = {
-//     name: "Yeha",
-//     age: "20",
-// }
+// // const player = {
+// //     name: "Yeha",
+// //     age: "20",
+// // }
 
-// console.log(player.age);
+// // console.log(player.age);
 
-// function plus(a,b) {
-//  console.log(a+b); 
+// // function plus(a,b) {
+// //  console.log(a+b); 
+// // };
+
+// // plus(3,5);
+
+
+
+
+// const calculater = {
+    
+//     add: function (a,b) { 
+//         return (a+b)
+//     },
+    
+
 // };
 
-// plus(3,5);
+// // calculater.add(3,4)  
+
+// console.log(calculater.add(3,4));
+ 
 
 
+// // function calculater(a,b) {
+// //  return a+b
+// // }
+
+// // calculater(3,5)
+ 
+
+// const age = 96;
+
+// function calculateKrAge(ageOfForeigner) {
+//    return ageOfForeigner + 2 
+// }
+
+// const krAge = calculateKrAge(age)
+
+// console.log(krAge);
 
 
 const calculater = {
     
-    add: function (a,b) { 
-        return (a+b)
+    add : function(a,b){
+        return a+b;
     },
     
+    divide : function(a,b){
+        return a/b;
+    },
+
+    minus : function(a,b){
+        return a-b;
+    },
 
 };
 
-// calculater.add(3,4)  
-
-console.log(calculater.add(3,4));
- 
-
-
-// function calculater(a,b) {
-//  return a+b
-// }
-
-// calculater(3,5)
- 
-
-const age = 96;
-
-function calculateKrAge(ageOfForeigner) {
-   return ageOfForeigner + 2 
-}
-
-const krAge = calculateKrAge(age)
-
-console.log(krAge);
+const addResult = calculater.add(10,5);
+const addResult = 
+console.log(addResult);
