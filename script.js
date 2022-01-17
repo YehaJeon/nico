a73a82ec0765525fd175a45b39de904a458b89c0
YehaJeon <aheynoez@gmail.com>
2022-01-17 17:25:41 +0900

    condition

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const age = prompt("How old are you?")
+// const age = prompt("How old are you?")
 
-console.log(age, parseInt(age))
+// console.log(age, parseInt(age))
 
 
-console.log(typeof "15", typeof parseInt("15"))
+// console.log(typeof "15", typeof parseInt("15"))
 
 const age = parseInt(prompt("How old are you"))
 
+// console.log(isNaN(age))
+
+if(isNaN(age)) {
+    console.log("Please write a number");
+} else { 
+    console.log("Thank you for writing your age.");
+}
