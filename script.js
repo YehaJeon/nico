f0efef83623e9ad1e5999cc21afd10f9f5e74a1e
YehaJeon <aheynoez@gmail.com>
2022-01-19 19:09:49 +0900

    이해중

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-document.title = "nico"
+// document.title = "nico"
 
-const title = document.getElementById("title")
+// const title= document.getElementById("title")
 
-title.innerText = "goo"
+// title.innerText = "goo"
 
- console.log(title.innerText)
+//  console.log(title.innerText)
+
+const hellos = document.getElementsByClassName("hello")
+
+console.log(hellos)
+   
