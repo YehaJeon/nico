56ce4575c330573bb89928acd5512a93e3510faa
YehaJeon <aheynoez@gmail.com>
2022-01-20 21:28:59 +0900

    toggle

--- script.js
+++ script.js
@@ -78,7 +78,13 @@ function handleTitleClick() {
  function handleTitleClick() {
     const clickedClass = "clicked";
     if (h1.classList.contains(clickedClass)){
-        
+        h1.classList.remove(clickedClass);
+    }else {
+        h1.classList.add(clickedClass);
     }
     
+ }
+
+ function handleTitleClick() {
+    h1.classList.toggle("clicked");
  }
